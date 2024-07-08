79004a8df9d0018ebced95fd0c395d6a8574c01d
wala oueslati <167649437+wala-oueslati@users.noreply.github.com>
2024-07-08 12:10:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let clearButton = document.querySelector('button');
 let errorMessage = document.querySelector('#error-message');
 
 input.addEventListener("keyup", function(event){
-    if(event.key == "Enter"){
+    if(event.key === "Enter"){
         if(this.value.trim() === "") {
             showError("Task is empty");
         } else {
@@ -19,19 +19,25 @@ input.addEventListener("keyup", function(event){
 
 let addItem = (input) => {
     let newItem = document.createElement("li");
-    newItem.innerHTML = `${input}<i>X</i>`;
+    newItem.innerHTML = `<input class="checkbox" type="checkbox" id="checkbox"></input>
+    <label for="checkbox"> </label>
+    &#160&#160&#160
+    ${input} 
+    <i>X</i>`;
     attachItemEvents(newItem);
     newlist.appendChild(newItem);
 };
 
 let attachItemEvents = (item) => {
-    item.addEventListener("click", function(){
-        if (this.parentElement === newlist) {
-            doinglist.appendChild(this);
+    const checkbox = item.querySelector('input[type="checkbox"]');
+    checkbox.addEventListener("change", function(){
+        if (this.checked && item.parentElement===newlist) {
+            doinglist.appendChild(this.parentElement);
         }
     });
 
-    item.querySelector("i").addEventListener("click", function(event){
+    const deleteIcon = item.querySelector("i");
+    deleteIcon.addEventListener("click", function(event){
         event.stopPropagation();
         let taskItem = this.parentElement;
         if (taskItem.parentElement === newlist || taskItem.parentElement === doinglist) {
